4f2654e74756ab68be3e1d55ba3d3984a61b7cb2
emulsazib <emul.sazib@northsouth.edu>
2026-04-27 18:41:39 +0600

    chapter 3 Complete.
    Generating parse tree.

--- src/comlang/main.cpp
+++ src/comlang/main.cpp
@@ -24,8 +24,14 @@ int main() {
   fprintf(stderr, "ready> ");
   getNextToken();
 
+  // Make the module, which holds all the code.
+  InitializeModule();
+
   // Run the main "interpreter loop" now.
   MainLoop();
+
+  // Print out all of the generated code.
+  TheModule->print(errs(), nullptr);
 
   return 0;
 }
